15118bbc1183f674c4a1a193e5fc440c668d0909
muzuiget <muzuiget@gmail.com>
2017-03-21 17:23:25 +0800

    update

--- install.travis.sh
+++ install.travis.sh
@@ -3,7 +3,7 @@
 case "$MARE_ARCH" in
     linux-x86)
         sudo apt-get -qq update
-        sudo apt-get install -y libc6-dev-i386
+        sudo apt-get install -y libc6-dev-i386 lib32readline6-dev
         ;;
     win-x64)
         sudo apt-get -qq update
