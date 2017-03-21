b337968321b5771029c44979be82648c1b9bb245
muzuiget <muzuiget@gmail.com>
2017-03-21 11:42:52 +0800

    mingw

--- build.sh
+++ build.sh
@@ -7,6 +7,23 @@ if [ $MARE_TARGET = 'macos-x64' ]; then
     make macosx
     cd ..
     gcc -O2 -bundle -undefined dynamic_lookup -I./lua-src/src -o remotedebug.so remotedebug.c
+elif [ $MARE_TARGET = 'win-x64' ]; then
+    sed -i 's/^\(CC\|AR\|RANLIB\)= /\0x86_64-w64-mingw32-/g' Makefile
+    make mingw
+    cd ..
+    x86_64-w64-mingw32-gcc -O2 -shared \
+        -I./lua-src/src \
+        -o remotedebug.dll remotedebug.c \
+        -L./lua-src/src -llua53
+elif [ $MARE_TARGET = 'win-x86' ]; then
+    apt install gcc-mingw-w64-i686
+    sed -i 's/^\(CC\|AR\|RANLIB\)= /\0xi686-w64-mingw32-/g' Makefile
+    make mingw
+    cd ..
+    x86_64-w64-mingw32-gcc -O2 -shared \
+        -I./lua-src/src \
+        -o remotedebug.dll remotedebug.c \
+        -L./lua-src/src -llua53
 else
     make linux
     cd ..
