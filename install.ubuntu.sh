963bf71d34bab2c5cfacbd02b9865b67314ff60b
muzuiget <muzuiget@gmail.com>
2017-03-21 16:59:12 +0800

    fix

--- install.ubuntu.sh
+++ install.ubuntu.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 case "$MARE_ARCH" in
+    linux-x86)
+        sudo apt-get -qq update
+        sudo apt-get install -y libc6-dev-i386
+        ;;
     win-x64)
         sudo apt-get -qq update
         sudo apt-get install -y gcc-mingw-w64-x86-64
