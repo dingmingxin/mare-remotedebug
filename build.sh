e85b3da20364c0253fe2a48301a8ac11f8795458
muzuiget <muzuiget@gmail.com>
2017-03-21 11:45:58 +0800

    update

--- build.sh
+++ build.sh
@@ -2,15 +2,16 @@
 
 git clone --depth 1 https://github.com/muzuiget/mirror-lua.git lua-src
 echo $MARE_TARGET
-cd lua-src
+cd lua-src/src
 if [ $MARE_TARGET = 'macos-x64' ]; then
     make macosx
-    cd ..
+    cd ../..
     gcc -O2 -bundle -undefined dynamic_lookup -I./lua-src/src -o remotedebug.so remotedebug.c
 elif [ $MARE_TARGET = 'win-x64' ]; then
+    apt install gcc-mingw-w64-x86-64
     sed -i 's/^\(CC\|AR\|RANLIB\)= /\0x86_64-w64-mingw32-/g' Makefile
     make mingw
-    cd ..
+    cd ../..
     x86_64-w64-mingw32-gcc -O2 -shared \
         -I./lua-src/src \
         -o remotedebug.dll remotedebug.c \
@@ -19,7 +20,7 @@ elif [ $MARE_TARGET = 'win-x86' ]; then
     apt install gcc-mingw-w64-i686
     sed -i 's/^\(CC\|AR\|RANLIB\)= /\0xi686-w64-mingw32-/g' Makefile
     make mingw
-    cd ..
+    cd ../..
     x86_64-w64-mingw32-gcc -O2 -shared \
         -I./lua-src/src \
         -o remotedebug.dll remotedebug.c \
